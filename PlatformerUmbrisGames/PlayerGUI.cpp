b54672311528eea77d7f6139c86d8e5c4c861f2c
Siebe Hoogenboom <13122703@student.hhs.nl>
2020-05-09 11:13:17 +0200

    Made the hp bar

--- PlatformerUmbrisGames/PlayerGUI.cpp
+++ PlatformerUmbrisGames/PlayerGUI.cpp
@@ -1,11 +1,36 @@
 #include "stdafx.h"
 #include "PlayerGUI.hpp"
 
+//initializers
+void PlayerGUI::initFont()
+{
+	this->font.loadFromFile("Fonts/slkscr.ttf");
+}
+
+void PlayerGUI::initHpBar()
+{
+	float width = 300.f;
+	float height = 25.f;
+	float x = 20.f;
+	float y = 20.f;
+	this->hpBarMaxWidth = width;
+
+	this->hpBarBack.setSize(sf::Vector2f(width, height));
+	this->hpBarBack.setFillColor(sf::Color(50, 50, 50, 200));
+	this->hpBarBack.setPosition(x, y);
+
+	this->hpBarInside.setSize(sf::Vector2f(width, height));
+	this->hpBarInside.setFillColor(sf::Color(250, 20, 20, 200));
+	this->hpBarInside.setPosition(this->hpBarBack.getPosition());
+}
 
 //Constructor/Destructor
 PlayerGUI::PlayerGUI(Player* player)
 {
 	this->player = player;
+
+	this->initFont();
+	this->initHpBar();
 }
 
 PlayerGUI::~PlayerGUI()
@@ -13,14 +38,33 @@ PlayerGUI::~PlayerGUI()
 
 }
 
+void PlayerGUI::updateHpBar()
+{
+	float percent = static_cast<float>(this->player->getAttributeComponent()->hp) / 
+		static_cast<float>(this->player->getAttributeComponent()->hpMax);
+
+	this->hpBarInside.setSize(
+		sf::Vector2f(
+			static_cast<float>(std::floor(this->hpBarMaxWidth * percent)), 
+			this->hpBarInside.getSize().y
+		)
+	);
+}
+
 void PlayerGUI::update(const float& deltaTime)
 {
+	this->updateHpBar();
+}
 
+void PlayerGUI::renderHpBar(sf::RenderTarget& target)
+{
+	target.draw(this->hpBarBack);
+	target.draw(this->hpBarInside);
 }
 
 void PlayerGUI::render(sf::RenderTarget& target)
 {
-
+	this->renderHpBar(target);
 }
 
 //Functions
--- PlatformerUmbrisGames/PlayerGUI.hpp
+++ PlatformerUmbrisGames/PlayerGUI.hpp
@@ -3,11 +3,21 @@
 #include "Player.hpp"
 
 class Player;
+class sf::RectangleShape;
 
 class PlayerGUI
 {
 private:
 	Player* player;
+
+	sf::Font font;
+	float hpBarMaxWidth;
+	sf::RectangleShape hpBarBack;
+	sf::RectangleShape hpBarInside;
+
+	//initializers
+	void initFont();
+	void initHpBar();
 
 public:
 	//Constructor/Destructor
@@ -15,7 +25,10 @@ public:
 	virtual ~PlayerGUI();
 
 	//Functions
+	void updateHpBar();
 	void update(const float& deltaTime);
+
+	void renderHpBar(sf::RenderTarget& target);
 	void render(sf::RenderTarget& target);
 };
 
